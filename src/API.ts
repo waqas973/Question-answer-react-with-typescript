e387c41d8a1922c5d73c259a5f766a23b8c17799
waqas973 <waqascomsts786@gmail.com>
2022-03-16 15:14:40 +0500

    commit

---
+++ src/API.ts
@@ -0,0 +1,33 @@
+import { shuffleArray } from "./utilis";
+
+export type Question = {
+  category: string;
+  correct_answer: string;
+  difficulty: string;
+  incorrect_answers: string[];
+  question: string;
+  type: string;
+};
+
+export type QuestionState = Question & { answers: string[] };
+
+export enum Difficulty {
+  EASY = "easy",
+  MEDIUM = "medium",
+  HARD = "hard",
+}
+
+export const fetchQuizQuestions = async (
+  amount: number,
+  difficult: Difficulty
+) => {
+  const endPoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficult}&type=multiple`;
+  const data = await (await fetch(endPoint)).json();
+  return data.results.map((question: Question) => ({
+    ...question,
+    answers: shuffleArray([
+      ...question.incorrect_answers,
+      question.correct_answer,
+    ]),
+  }));
+};
